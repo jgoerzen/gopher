014a89813b6b7571d176c358803224acb84fabff
jgoerzen <jgoerzen>
2002-03-21 08:49:51 +0100

    /gopher/head: changeset 130
    Added copyright

--- mime/mimetypes.c
+++ mime/mimetypes.c
@@ -1,11 +1,51 @@
+/*
+
+mimetypes.c
+Copyright (C) 2002  John Goerzen <jgoerzen@complete.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #define __GOPHER_MIME_MIMETYPES_C__
 
 #include <glib.h>
+
+#ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
+#endif
+#ifdef HAVE_SYS_STAT_H
 #include <sys/stat.h>
+#endif
+#ifdef HAVE_FCNTL_H
 #include <fcntl.h>
-#include <stdio.h>
+#endif
+
+/* If the above didn't get our prototypes, try a few others just to be safe. */
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifdef HAVE_STRING_H
 #include <string.h>
+#else
+#ifdef HAVE_STRINGS_H
+#include <strings.h>
+#endif
+#endif
+
 #include "mimetypes.h"
 
 
