f5f9feb5076a6c3ad567e49d74cb70c47ec59de5
jgoerzen <jgoerzen>
2002-03-21 08:31:14 +0100

    /gopher/head: changeset 129
    Added.

--- mime/mimetypes.c
+++ mime/mimetypes.c
@@ -1,31 +1,86 @@
+#define __GOPHER_MIME_MIMETYPES_C__
+
 #include <glib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include "mimetypes.h"
 
-void safeinsert(GHashTable *table, gpointer key, gpointer value);
-static void print_entry(gpointer key, gpointer value, gpointer user_data);
 
-void main(void) {
-        GScanner *scanner;
-        GScannerConfig config;
+static GHashTable *types = NULL, *encodings = NULL;
+
+char *getmimetype(char *filename, char *defaulttype) {
+        char *mimetype, *encoding;
+        getmimetypes(filename, &mimetype, &encoding, defaulttype);
+        return mimetype;
+}
+
+char *getencoding(char *filename) {
+        char *mimetype, *encoding;
+        getmimetypes(filename, &mimetype, &encoding, "DEFAULTTYPE");
+        return encoding;
+}
+
+void getmimetypes(char *filename, char **mimetype,
+                  char **encoding, char *defaulttype) {
+        gchar *ext;
+        gchar *myfilename;
+
+        /* Initialize to defaults. */
+        *mimetype = defaulttype;
+        *encoding = NULL;
+
+        myfilename = g_strdup(filename);
+        ext = strrchr(myfilename, '.');
+        if (ext) {              /* We have an extension. */
+                *ext = 0;       /* Set it to null for future lookups. */
+                ext++;          /* Advance to actual string */
+                if ((*encoding = g_hash_table_lookup(encodings, ext))) {
+                        /* OK, got an encoding.  Now reset for base type
+                           lookup. */
+                        ext = strrchr(myfilename, '.');
+                }
+                if (ext) {      /* Check again -- maybe had enc but no type */
+                        gchar *type;
+                        ext++;
+                        type = g_hash_table_lookup(types, ext);
+                        if (type) {
+                                *mimetype = type;
+                        }
+                }
+        }
+        g_free(myfilename);
+}
+
+void scanmimetypes(char *filename) {
+        scanfile(filename, &types);
+}
+
+void scanencodings(char *filename) {
+        scanfile(filename, &encodings);
+}
+
+void scanfile(char *filename, GHashTable **tableptr) {
+        int fd;
         GHashTable *table;
+        GScanner *scanner;
         GString *curmimetype = NULL;
+        GScannerConfig config;
 
-        int filefd;
-        
-        curmimetype = g_string_new(NULL);
+        fd = open(filename, O_RDONLY);
+        if (fd < 0) return;
 
-        /* Create the table. */
-        table = g_hash_table_new(g_str_hash, g_str_equal);
+        /* Initialize the table if needed. */
 
-        filefd = open("/etc/mime.types", O_RDONLY);
-        if (filefd < 0) exit(1);
-        
-        /* Create the scanner. */
-        
+        table = *tableptr;
+        if (! table) {
+                *tableptr = g_hash_table_new(g_str_hash, g_str_equal);
+                table = *tableptr;
+        }
+
+        /* Configure the scanner. */
         config.cset_skip_characters = " \t\n";
         config.cpair_comment_single = "#\n";
         config.cset_identifier_first = G_CSET_a_2_z "_" G_CSET_A_2_Z;
@@ -33,8 +88,11 @@ void main(void) {
         config.scan_comment_multi = 0;
         config.cset_identifier_nth = G_CSET_a_2_z G_CSET_A_2_Z G_CSET_LATINS
                 G_CSET_LATINC "_-/0123456789.+";
+
+        /* Create the scanner. */
+        
         scanner = g_scanner_new(&config);
-        g_scanner_input_file(scanner, filefd);
+        g_scanner_input_file(scanner, fd);
 
         /* Scan.  */
         while (g_scanner_peek_next_token(scanner) != G_TOKEN_EOF) {
@@ -64,20 +122,14 @@ void main(void) {
                 }
         }
 
-        
-
-        g_hash_table_foreach(table, print_entry, NULL);
         g_scanner_destroy(scanner);
-        g_hash_table_destroy(table);
 }
 
 void safeinsert(GHashTable *table, gpointer key, gpointer value) {
-        char *old_value, *old_key;
+        gpointer old_value, old_key;
 
         /* If already there... */
         if (g_hash_table_lookup_extended(table, key, &old_key, &old_value)) {
-                printf("WARN: Replacing %s = %s with %s = %s\n",
-                       old_key, old_value, key, value);
                 /* Insert new value. */
                 g_hash_table_insert(table, g_strdup(key), g_strdup(value));
                 g_free(old_key);
@@ -87,7 +139,3 @@ void safeinsert(GHashTable *table, gpointer key, gpointer value) {
         }
 }
 
-                
-static void print_entry(gpointer key, gpointer value, gpointer user_data) {
-        printf("%s: %s\n", key, value);
-}
