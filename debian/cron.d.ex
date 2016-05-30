9c19e30e75946fabce6ea61696332372cd9527b3
John Goerzen <jgoerzen@complete.org>
2016-05-29 21:22:53 -0500

    Deleted example files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the gopher package
-#
-0 4	* * *	root	gopher_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(gopher):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="gopher" command="/usr/bin/gopher"
